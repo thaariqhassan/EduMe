71724d46af68b8ddeda70445145a64de9b735638
Thaariq Hassan <thaariq.hassan@gmail.com>
2026-01-11 12:35:24 +0530

    Course Card Created

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter,Routes,Route } from 'react-router'
+import Courses from './Courses.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route element={<App />} path='/'/>
+        <Route element={<Courses />} path='/courses'/>
       </Routes>
     </BrowserRouter>
   </StrictMode>
