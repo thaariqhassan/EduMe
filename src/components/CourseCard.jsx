f648e94a485447d2277677ee1e035764cd4c1aa0
Thaariq Hassan <thaariq.hassan@gmail.com>
2026-01-11 11:57:01 +0530

    Courses page initialized

---
+++ src/components/CourseCard.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function CourseCard() {
+  return (
+    <div className="flex flex-col h-40">
+        <div className="">
+            <img src="/image.png" alt="pen" className="size-50"/>
+        </div>
+    </div>
+  )
+}
+
+export default CourseCard
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,13 +6,13 @@ function Navbar() {
       <div className="text-2xl text-black">EduMe</div>
       <div className="flex flex-row justify-between items-center min-w-25 w-125">
           <div className='cursor-pointer' style={{fontFamily:"poppins"}}>Subjects</div>
-          <div className='cursor-pointer'style={{fontFamily:"poppins"}}>courses</div>
+          <div className='cursor-pointer'style={{fontFamily:"poppins"}}>Courses</div>
           <div className='cursor-pointer'style={{fontFamily:"poppins"}}>Degrees</div>
           <div className='cursor-pointer'style={{fontFamily:"poppins"}}>For Business</div>
       </div>
-      <div className="flex w-37.5 flex-row justify-between items-center">
+      <div className="flex w-40 flex-row justify-between items-center">
         <div className="bg-white text-black rounded-md p-2 cursor-pointer">Sign up</div>
-        <div className="bg-red-500 text-white rounded-md p-2 cursor-pointer">Login</div>
+        <div className="bg-red-500 text-white rounded-md p-2 px-4 cursor-pointer">Login</div>
       </div>
     </div>
     
