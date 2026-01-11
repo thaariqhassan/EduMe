05faef6be300669ab1191746cc4e693a45db0fbd
Abhay Murali M <abhaymuralim9@gmail.com>
2026-01-11 12:35:11 +0530

    login and signin created

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,16 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter,Routes,Route } from 'react-router'
+import Login from './Login.jsx'
+import Signup from "./Signup.jsx";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route element={<App />} path='/'/>
+        <Route element={<Login />} path='/login'/>
+        <Route element={<Signup />} path='/signup'/>
       </Routes>
     </BrowserRouter>
   </StrictMode>
