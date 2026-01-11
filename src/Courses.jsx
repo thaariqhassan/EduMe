7649aceb9df9fbeba9dcff55221198a399d780a1
Thaariq Hassan <thaariq.hassan@gmail.com>
2026-01-11 12:47:34 +0530

    Cources page roghly made

--- src/Courses.jsx
+++ src/Courses.jsx
@@ -3,9 +3,19 @@ import CourseCard from './components/CourseCard'
 
 function Courses() {
   return (
-    <div>
-      <CourseCard/>
+    <div className="flex flex-col items-center justify-between gap-10 m-5">
+        <div className=" w-100 h-10 shadow border border-gray-300 rounded-2xl">
+            <input placeholder='Enter Cources'/>
+        </div>
+        <div className='flex flex-wrap gap-10'>
+        <CourseCard/>
+        <CourseCard/>
+        <CourseCard/>
+        <CourseCard/>
+        <CourseCard/>
+        </div>
     </div>
+    
   )
 }
 
